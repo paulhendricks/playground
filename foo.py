4d11f5cf0b2137a3d7f8f83bf08f00eab4837d7d
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-05 16:34:48 -0400

    added some sample code

--- foo.py
+++ foo.py
@@ -1,9 +1,24 @@
-#!/usr/bin/python3
 """TO BE EDITED
 
 Not Complete!
 """
+import gym
+
+
+def main():
+    env = gym.make('CartPole-v0')
+    env.reset()
+    for i_episode in xrange(20):
+        observation = env.reset()
+        for t in xrange(100):
+            env.render()
+            print observation
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                print "Episode finished after {} timesteps".format(t + 1)
+                break
 
 
 if __name__ == '__main__':
-    print("Hello world!")
+    main()
