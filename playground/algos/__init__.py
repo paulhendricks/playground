155f950d4e7166a09a9eea7daa7b0637c7100e9a
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-01 17:15:40 -0400

    updated init

--- playground/algos/__init__.py
+++ playground/algos/__init__.py
@@ -1,10 +1,12 @@
 from . import agent
+from . import gradient
 from . import blind
 from . import local
 from . import random
 
 __all__ = []
 __all__ = agent.__all__
+__all__ = gradient.__all__
 __all__ = blind.__all__
 __all__ = local.__all__
 __all__ = random.__all__
