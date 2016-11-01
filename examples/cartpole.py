e9e167d41ac3a1c99265bc5ca0854a10c84457f3
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-01 17:13:53 -0400

    updated example

--- examples/cartpole.py
+++ examples/cartpole.py
@@ -1,5 +1,6 @@
 import gym
 from playground.algos.blind import MonteCarloAgent
+from playground.algos.gradient import PolicyGradientAgent
 from playground.algos.local import HillClimbingAgent, SimulatedAnnealingAgent
 from playground.algos.random import RandomAgent
 from playground.experiment import run_experiment
@@ -13,15 +14,20 @@ result = run_experiment(env, agent)
 
 # Run monte carlo agent
 agent = MonteCarloAgent(env.observation_space, env.action_space)
-result = run_experiment(env, agent, episode_count=1000, watch=False)
+result = run_experiment(env, agent, episode_count=5000, watch=False)
 run_experiment(env, result)
 
 # Run hill climbing agent
 agent = HillClimbingAgent(env.observation_space, env.action_space)
-result = run_experiment(env, agent, episode_count=1000, watch=False)
+result = run_experiment(env, agent, episode_count=5000, watch=False)
 run_experiment(env, result)
 
 # Run simulated annealing agent
 agent = SimulatedAnnealingAgent(env.observation_space, env.action_space)
+result = run_experiment(env, agent, episode_count=5000, watch=False)
+run_experiment(env, result)
+
+# Run policy gradient agent
+agent = PolicyGradientAgent(env.observation_space, env.action_space)
 result = run_experiment(env, agent, episode_count=5000, watch=False)
 run_experiment(env, result)
